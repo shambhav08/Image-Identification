4b523ed20b0cb850e51b94a2dc023339a69ac3a0
shambhav08 <77199331+shambhav08@users.noreply.github.com>
2021-04-29 20:56:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function setup()
+{
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(350, 300);
+    video.hide();
+}
+
+function draw()
+{
+    image(video, 0, 0, 350, 300);
+}
